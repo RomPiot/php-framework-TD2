fc46b60554872d0ed6ff58aaeefeda21b0a94c2d
Romain PIOT <romainpiot.pro@gmail.com>
2020-01-27 14:28:53 +0100

    base of framework

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    public function index(Request $request) : Response {
+
+        $response = new Response("it works !");
+        $response->send();
+
+
+    }
+
+}
